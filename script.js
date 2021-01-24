e4642d3b7f5ef9f603f22df37509583fa8bc8042
Zeetbor <rob-terrins@hotmail.co.uk>
2021-01-24 14:31:57 +0000

    Added extra buttons and functionality

---
+++ script.js
@@ -0,0 +1,150 @@
+const gridContainer = document.getElementById('grid-container');
+
+const newButton = document.getElementById("new");
+const rainbowButton = document.getElementById("rainbow");
+const greyButton = document.getElementById("greyscale");
+const blackButton = document.getElementById("black");
+const eraseButton = document.getElementById("erase");
+const lineButton = document.getElementById("lines");
+const clearButton = document.getElementById('clear');
+const colorInput = document.getElementById('input-color')
+const chooseButton = document.getElementById('choose')
+
+// Create first grid
+
+function addDivs(numberOfDivs) {
+  var newElement = [];
+  var numberOfDivs = prompt("Select grid size between 4 and 128", 32);
+  if (numberOfDivs > 3 && numberOfDivs < 129) {
+    for (i = 0; i < numberOfDivs * numberOfDivs; i++) {
+      newElement[i] = document.createElement('div');
+      newElement[i].className = 'grid-segment';
+      newElement[i].classList.add('gridlines');
+      newElement[i].id = (i + 1);
+      //newElement[i].textContent = 'this is div number: ' + (i + 1);
+      gridContainer.appendChild(newElement[i]);
+      gridContainer.style.gridTemplateColumns = `repeat(${numberOfDivs}, 1fr)`;
+      gridContainer.style.gridTemplateRows = `repeat(${numberOfDivs}, 1fr)`;
+    }
+  } else {
+    alert("Please pick a number between 4 and 128")
+    addDivs();
+  }
+}
+addDivs();
+
+// Add functions for buttons
+
+function newGrid() {
+  let gridSegments = document.querySelectorAll('div.grid-segment');
+  gridSegments.forEach(div => {
+    gridContainer.removeChild(div);
+  });
+}
+
+
+function rainbowColor() {
+  let gridSegments = document.querySelectorAll('div.grid-segment');
+  gridSegments.forEach(div => {
+    div.addEventListener("mouseover", function() {
+      div.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    })
+  })
+}
+
+function greyHex() {
+  //bitwise OR. Gives value in the range 0-255 which is then converted to base 16 (hex).
+  let v = (Math.random() * (256) | 0).toString(16);
+  return "#" + v + v + v;
+};
+
+function greyColor() {
+  let gridSegments = document.querySelectorAll('div.grid-segment');
+  gridSegments.forEach(div => {
+    div.addEventListener("mouseover", function() {
+      div.style.backgroundColor = greyHex();
+    })
+  })
+}
+
+function blackColor() {
+  let gridSegments = document.querySelectorAll('div.grid-segment');
+  gridSegments.forEach(div => {
+    div.addEventListener("mouseover", function() {
+      div.style.backgroundColor = '#000';
+    })
+  })
+}
+
+function erase() {
+  let gridSegments = document.querySelectorAll('div.grid-segment');
+  gridSegments.forEach(div => {
+    div.addEventListener("mouseover", function() {
+      div.style.backgroundColor = '#fff';
+    })
+  })
+}
+
+function removeLines() {
+  let gridSegments = document.querySelectorAll('div.grid-segment');
+  gridSegments.forEach(segment => {
+    segment.classList.toggle('gridlines');
+  })
+}
+
+function clear() {
+  let gridSegments = document.querySelectorAll('div.grid-segment');
+  gridSegments.forEach(segment => {
+    segment.style.backgroundColor = "white";
+  })
+};
+
+function userColor(chosenColor) {
+  let gridSegments = document.querySelectorAll('div.grid-segment');
+  gridSegments.forEach(div => {
+    div.addEventListener("mouseover", function() {
+      div.style.backgroundColor = chosenColor;
+    })
+  })
+}
+
+// Add event listeners to buttons
+
+newButton.addEventListener("click", () => {
+  newGrid();
+  addDivs();
+});
+
+rainbowButton.addEventListener("click", () => {
+  rainbowColor();
+})
+
+greyButton.addEventListener("click", () => {
+  greyColor();
+})
+
+blackButton.addEventListener("click", () => {
+  blackColor();
+})
+
+eraseButton.addEventListener("click", () => {
+  erase();
+})
+
+lineButton.addEventListener("click", () => {
+  removeLines();
+});
+
+clearButton.addEventListener("click", () => {
+  clear();
+})
+
+chooseButton.addEventListener('click', () => {
+  let chosenColor = colorInput.value;
+  userColor(chosenColor);
+})
+
+colorInput.addEventListener('change', () => {
+  let chosenColor = colorInput.value;
+  userColor(chosenColor);
+})
